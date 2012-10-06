adf98f0f6de6985f6cadba81600320262d3e690c
Andrew Dean <adean@thoughtworks.com>
2012-10-06 01:24:45 -0500

    Cleaning up project file.

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject map_reduce_kata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Solution for HOD map reduce kata"
   :dependencies [[speclj "2.3.1"] [org.clojure/clojure "1.4.0"]]
   :plugins [[speclj "2.3.1"]]
   :test-paths ["spec/"])
