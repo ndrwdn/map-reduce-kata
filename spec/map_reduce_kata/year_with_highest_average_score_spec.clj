659e0c16c587091c9b9d518ae9a6691fdde33d11
Andrew Dean <adean@thoughtworks.com>
2012-10-06 00:55:07 -0500

    Find year with highest average movie score.

---
+++ spec/map_reduce_kata/year_with_highest_average_score_spec.clj
@@ -0,0 +1,15 @@
+(ns map-reduce-kata.year-with-highest-average-score-spec
+    (:use
+        [speclj.core]
+        [map-reduce-kata.year-with-highest-average-score]))
+
+(describe "Finds year with highest movie score."
+  (it "Highest movie score in small data set."
+      (let [highest-scoring-year (highest-average-movie-score "movies.txt")]
+        (should= :1972 (first highest-scoring-year))
+        (should= 9.2 (last highest-scoring-year))))
+
+  (it "Highest movie score in large data set."
+      (let [highest-scoring-year (highest-average-movie-score "ratings.txt")]
+        (should= :1889 (first highest-scoring-year))
+        (should= 8.0 (last highest-scoring-year)))))
---
+++ src/map_reduce_kata/year_with_highest_average_score.clj
@@ -0,0 +1,27 @@
+(ns map-reduce-kata.year-with-highest-average-score
+  (:require
+    [map-reduce-kata.movie-data-source :as s]
+    [map-reduce-kata.average-movie-scores-by-year :as avg]))
+
+(defn- compare-scores
+  "Compares two year's scores."
+  [highest-year-score year-score]
+  (let [current-score (last year-score)
+
+        highest-score (or (last highest-year-score) 0.0)
+
+        new-highest-score (if (> current-score highest-score)
+                            year-score
+                            highest-year-score)]
+    new-highest-score))
+
+(defn- find-highest-scoring-year
+  "Maps score and year to dummy key value."
+  [average-scores]
+  (reduce compare-scores [] average-scores))
+
+(defn highest-average-movie-score
+  "Gets a hashmap of year to average movie score for that year."
+  [fname]
+  (let [average-scores-by-year (avg/average-movie-scores fname)]
+    (find-highest-scoring-year average-scores-by-year)))
